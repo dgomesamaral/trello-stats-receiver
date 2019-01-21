84b3ccbb6bc7e6d528426674dc3156e6bd7778d3
Wylock Sven <sven.wylock.987@infrabel.be>
2019-01-21 13:51:49 +0100

    change on index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const app = express()
+const port = 3000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', (req, res) => {
+  console.log(req)
+  res.send("Received your message")
+})
+
+app.listen(port, () => console.log(`App listening on port ${port}`))
