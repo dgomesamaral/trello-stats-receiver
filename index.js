e6c4ce8ac10131cb6e099724ffdbb2ca3b2c6b94
Wylock Sven <sven.wylock.987@infrabel.be>
2019-01-21 14:47:39 +0100

    routing added

--- index.js
+++ index.js
@@ -11,8 +11,20 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const port = 3000
 
+
 app.get('/', urlencodedParser, (req, res) => {
-  console.log("Name is ", req.body.action.data.card.name)
+  LoggingUtil('logs_POST.txt', req.body)
 })
 
+app.post('/', urlencodedParser, (req, res) => {
+  LoggingUtil('logs_GET.txt', req.body.action.data.card.name)
+})
+
+
 app.listen(process.env.PORT || 5000, () => console.log(`App listening on port ${port}`))
+
+function LoggingUtil(file, content) {
+	fs.appendFile(file, content + '\n', (err) => {
+		if (err) throw err;
+	}) 
+}
