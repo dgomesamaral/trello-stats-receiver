61a96c34811b69834d8994f8c7bd0b703f39aeb3
Wylock Sven <sven.wylock.987@infrabel.be>
2019-01-21 14:32:33 +0100

    bodyparser added

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 const port = 3000
 
-app.get('/', (req, res) => {
-  console.log(req.body)
+app.get('/', urlencodedParser, (req, res) => {
+  console.log("Name is ", req.body.action.data.card.name)
 })
 
 app.listen(process.env.PORT || 5000, () => console.log(`App listening on port ${port}`))
