f46e6fa8a0cd430ce0d9c2f66d0ba8460b3cd01c
Wylock Sven <sven.wylock.987@infrabel.be>
2019-01-21 14:22:00 +0100

    printing body

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  console.log(req)
-  res.send("Received your message")
+  console.log(req.body)
 })
 
 app.listen(process.env.PORT || 5000, () => console.log(`App listening on port ${port}`))
