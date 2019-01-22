c536873c8da2788a71818144053b45418300e2b5
Wylock Sven <sven.wylock.987@infrabel.be>
2019-01-22 09:42:44 +0100

    added endpoints

--- index.js
+++ index.js
@@ -9,15 +9,33 @@ const port = 3000
 // Allows us to easily read the payload from the webhook
 app.use( bodyParser.json() );
 
-app.use("/", (req, res, next) => {
+app.use("/analyzing", (req, res, next) => {
   console.log(req.body.action);
-  
+  console.log(req.body.model);
+  res.sendStatus(200);
+});
+
+app.use("/developing", (req, res, next) => {
+  console.log(req.body.action);
+  console.log(req.body.model);
+  res.sendStatus(200);
+});
+
+app.use("/testing-qua", (req, res, next) => {
+  console.log(req.body.action);
+  console.log(req.body.model);
+  res.sendStatus(200);
+});
+
+app.use("/testing-acc", (req, res, next) => {
+  console.log(req.body.action);
+  console.log(req.body.model);
   res.sendStatus(200);
 });
 
 app.listen(process.env.PORT || 5000, () => console.log(`App listening on port ${port}`))
 
-function LoggingUtil(file, content) {
+function registerActivity(file, content) {
 	fs.appendFile(file, content + '\n', (err) => {
 		if (err) throw err;
 	}) 
